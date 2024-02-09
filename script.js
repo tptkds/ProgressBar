23ac51e356152f1bd5e074a871d27ab13443a8dc
KimYouGyoung <tptkds12@gmail.com>
2024-02-09 22:44:25 +0900

    프로그레스바 구현 완료

--- script.js
+++ script.js
@@ -4,4 +4,17 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $progressBar = get('.progress-bar');
+  
+  const onScroll = () => {
+    const height =
+      document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    
+    const scrollTop = document.documentElement.scrollTop;
+
+    const width = (scrollTop / height) * 100;
+    $progressBar.style.width = width + '%';
+  }
+  window.addEventListener('scroll', () => onScroll())
 })()
